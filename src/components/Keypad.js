d3551be70d062f6f5cb0e3d1501ecae12959899d
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-09-12 10:54:06 -0400

    commit tree 97177946f7840b906dd35b2ab910fb738d1fa0b5

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function change(event){
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input onChange={change} type='password'></input>
+        </div>
     )
 }
 
